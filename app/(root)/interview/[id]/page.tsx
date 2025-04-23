b667aabbcfab97d9d8dbde8c7ac5401d4a2fe60c
Aayush Gupta <aayushgupta69official@gmail.com>
2025-04-24 00:40:00 +0530

    Completed the Interview Details page

---
+++ app/(root)/interview/[id]/page.tsx
@@ -0,0 +1,64 @@
+import Image from "next/image";
+import { redirect } from "next/navigation";
+
+import Agent from "@/components/agent";
+import { getRandomInterviewCover } from "@/lib/utils";
+
+import {
+  getFeedbackByInterviewId,
+  getInterviewById,
+} from "@/lib/actions/general.action";
+import { getCurrentUser } from "@/lib/actions/auth.action";
+import DisplayTechIcons from "@/components/display-tech-icons";
+
+const InterviewDetails = async ({ params }: RouteParams) => {
+  const { id } = await params;
+
+  const user = await getCurrentUser();
+
+  const interview = await getInterviewById(id);
+  if (!interview) {
+    redirect("/");
+  }
+
+  const feedback = await getFeedbackByInterviewId({
+    interviewId: id,
+    userId: user?.id!,
+  });
+
+  return (
+    <>
+      <div className="flex flex-row justify-between gap-4">
+        <div className="flex flex-row items-center gap-4 max-sm:flex-col">
+          <div className="flex flex-row items-center gap-4">
+            <Image
+              src={getRandomInterviewCover()}
+              alt="cover-image"
+              width={40}
+              height={40}
+              className="size-[40px] rounded-full object-cover"
+            />
+            <h3 className="capitalize">{interview.role} Interview</h3>
+          </div>
+
+          <DisplayTechIcons techStack={interview.techstack} />
+        </div>
+
+        <p className="bg-dark-200 h-fit rounded-lg px-4 py-2">
+          {interview.type}
+        </p>
+      </div>
+
+      <Agent
+        userName={user?.name!}
+        userId={user?.id}
+        interviewId={id}
+        type="interview"
+        questions={interview.questions}
+        feedbackId={feedback?.id}
+      />
+    </>
+  );
+};
+
+export default InterviewDetails;
